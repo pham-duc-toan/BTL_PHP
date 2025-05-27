d0729720884045f9da8d9a7e4740c510f38330e1
Phạm Đức Toàn <phamductoan220704@gmail.com>
2025-05-27 10:36:57 +0700

    cart +address,  chua co order

--- api/add_address.php
+++ api/add_address.php
@@ -1,6 +1,12 @@
 <?php
 include_once __DIR__ . '/../helper/db.php';
+include_once __DIR__ . '/../helper/functions.php';
 session_start();
+
+if (!isset($_SESSION['user'])) {
+  echo json_encode(['success' => false, 'error' => "Session user chưa tồn tại."]);
+  exit;
+}
 
 $user_id = $_SESSION['user']['id'];
 $full_name = $_POST['full_name'] ?? '';
@@ -9,17 +15,21 @@ $address = $_POST['address'] ?? '';
 
 if (!$full_name || !$phone || !$address) {
   $_SESSION['error'] = "Vui lòng điền đầy đủ thông tin.";
-  header("Location: " . $_SERVER['HTTP_REFERER']);
+  echo json_encode(['success' => false, 'error' => $_SESSION['error']]);
   exit;
 }
+$id = generateId();
+$stmt = $conn->prepare("INSERT INTO addresses (id,user_id, full_name, phone, address) VALUES (?, ?, ?, ?,?)");
+$stmt->bind_param("sssss", $id, $user_id, $full_name, $phone, $address); // user_id là số nguyên
 
-$stmt = $conn->prepare("INSERT INTO addresses (user_id, full_name, phone, address) VALUES (?, ?, ?, ?)");
-$stmt->bind_param("ssss", $user_id, $full_name, $phone, $address);
 $success = $stmt->execute();
 
 if ($success) {
   $_SESSION['success'] = "Đã thêm địa chỉ mới!";
   echo json_encode(['success' => true, 'new_id' => $conn->insert_id]);
 } else {
-  echo json_encode(['success' => false, 'error' => "Không thể thêm địa chỉ."]);
+  echo json_encode([
+    'success' => false,
+    'error' => "Không thể thêm địa chỉ. Chi tiết: " . $stmt->error
+  ]);
 }
